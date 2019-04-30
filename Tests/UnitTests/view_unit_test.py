dddaa030baeda323c91f392fbe3811dea2444bff
sge0039 <simon.england0@xtra.co.nz>
2019-04-30 17:11:34 +1200

    Add View unit tests

--- Tests/UnitTests/view_unit_test.py
+++ Tests/UnitTests/view_unit_test.py
@@ -9,6 +9,22 @@ class ClassMakerTest(unittest.TestCase):
     def tearDown(self):
         pass
 
+    def test_01(self):
+        # Arrange
+        expected_result = '\n'
+        # Act
+        actual_result = View.newline()
+        # Assert
+        self.assertEqual(actual_result, expected_result, 'Expected to be a Newline')
+
+    def test_02(self):
+        # Arrange
+        expected_result = '    '
+        # Act
+        actual_result = View.tab()
+        # Assert
+        self.assertEqual(actual_result, expected_result, 'Expected to be four spaces')
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
