3ee935d4ca5e174fd98b318fdc665db2ab1f133c
Simon England <simon.england0@xtra.co.nz>
2019-05-09 15:08:03 +1200

    add tests for file_handler_unit_test

--- Tests/UnitTests/file_handler_unit_test.py
+++ Tests/UnitTests/file_handler_unit_test.py
@@ -22,6 +22,16 @@ class FileHandlerTest(unittest.TestCase):
 
     def test_02(self):
         # Arrange
+        file_handler = FileHandler()
+        expected_result = ['@startuml\n', 'class LevelEditor{\n', '  allMyBlocks\n', '  getBlock()\n', '}\n',
+                           '@enduml']
+        # Act
+        actual_result = file_handler.load_from_file()
+        # Assert
+        self.assertEqual(actual_result, expected_result, 'Expected to be a List of Strings')
+
+    def test_03(self):
+        # Arrange
         file_name = 'unit_test_load'
         file_content = 'class LevelEditor:\n    def __init__(self, block):\n        self.all_my_blocks = block\n\
 \n    def get_block(self):\n        pass\n'
@@ -33,6 +43,40 @@ class FileHandlerTest(unittest.TestCase):
         file_handler.get_folder_dir(file_dir)
         file_handler.write_to_file(file_name, file_content)
         actual_result = file_handler.load_from_file(file_dir + '/' + file_name + '.py')
+        # Assert
+        self.assertEqual(actual_result, expected_result, 'Expected to be a List of Strings')
+
+    def test_04(self):
+        # Arrange
+        file_name = 'unit_test_load'
+        file_content = 'class LevelEditor:\n    def __init__(self, block):\n        self.all_my_blocks = block\n\
+\n    def get_block(self):\n        pass\n'
+        file_dir = 'InputOutputFiles'
+        file_handler = FileHandler()
+        expected_result = ['class LevelEditor:\n', '    def __init__(self, block):\n',
+                           '        self.all_my_blocks = block\n', '\n', '    def get_block(self):\n',
+                           '        pass\n']
+        # Act
+        file_handler.get_folder_dir()
+        file_handler.write_to_file(file_name, file_content)
+        actual_result = file_handler.load_from_file(file_dir + '/' + file_name + '.py')
+        # Assert
+        self.assertEqual(actual_result, expected_result, 'Expected to be a List of Strings')
+
+    def test_05(self):
+        # Arrange
+        file_name = 'unit_test_load'
+        file_content = 'class LevelEditor:\n    def __init__(self, block):\n        self.all_my_blocks = block\n\
+\n    def get_block(self):\n        pass\n'
+        file_dir = 'InputOutputFiles'
+        file_handler = FileHandler()
+        expected_result = ['€\x03X|\x00\x00\x00class LevelEditor:\n', '    def __init__(self, block):\n',
+                           '        self.all_my_blocks = block\n', '\n', '    def get_block(self):\n',
+                           '        pass\n', 'q\x00.']
+        # Act
+        file_handler.get_folder_dir(file_dir)
+        file_handler.write_to_pickle(file_name, file_content)
+        actual_result = file_handler.load_from_file(file_dir + '/' + file_name + '.pickle')
         # Assert
         self.assertEqual(actual_result, expected_result, 'Expected to be a List of Strings')
 
